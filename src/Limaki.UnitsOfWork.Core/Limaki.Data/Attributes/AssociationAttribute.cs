8cbc7ae1b1be757a5b3f3491c4068978f8af43d9
lytico <lytico@users.sourceforge.net>
2020-11-12 19:33:04 +0100

    Limaki.UnitsOfWork.Core: add Data-Attributes first draft

---
+++ src/Limaki.UnitsOfWork.Core/Limaki.Data/Attributes/AssociationAttribute.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Limaki.Data.Attributes
+{
+	/// <summary>
+	/// Defines relation between tables or views.
+	/// Could be applied to:
+	/// - instance properties and fields;
+	/// - instance and static methods.
+	/// 
+	/// For associations, defined using static methods, <c>this</c> mapping side defined by type of first parameter.
+	/// Also, optionally, you can pass data context object as extra method parameter.
+	/// 
+	/// Based on association type - to one or to multiple records - result type should be target record's mapping type or
+	/// <see cref="IEquatable{T}"/> collection.
+	/// 
+	/// </summary>
+
+	[AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple=false)]
+	public class AssociationAttribute : Attribute
+	{
+		/// <summary>
+		/// Creates attribute instance.
+		/// </summary>
+		public AssociationAttribute()
+		{
+			CanBeNull = true;
+		}
+
+		/// <summary>
+		/// Gets or sets mapping schema configuration name, for which this attribute should be taken into account.
+		/// <see cref="ProviderName"/> for standard names.
+		/// Attributes with <c>null</c> or empty string <see cref="Configuration"/> value applied to all configurations (if no attribute found for current configuration).
+		/// </summary>
+		public string?      Configuration       { get; set; }
+
+		/// <summary>
+		/// Gets or sets comma-separated list of association key members on this side of association.
+		/// Those keys will be used for join predicate generation and must be compatible with <see cref="OtherKey"/> keys.
+		/// You must specify keys it you do not use custom predicate (see <see cref="ExpressionPredicate"/>).
+		/// </summary>
+		public string?      ThisKey             { get; set; }
+
+		/// <summary>
+		/// Gets or sets comma-separated list of association key members on another side of association.
+		/// Those keys will be used for join predicate generation and must be compatible with <see cref="ThisKey"/> keys.
+		/// You must specify keys it you do not use custom predicate (see <see cref="ExpressionPredicate"/>).
+		/// </summary>
+		public string?      OtherKey            { get; set; }
+
+		/// <summary>
+		/// Specifies static property or method without parameters, that returns join predicate expression. This predicate will be used together with
+		/// <see cref="ThisKey"/>/<see cref="OtherKey"/> join keys, if they are specified.
+		/// Predicate expression lambda function takes two parameters: this record and other record and returns boolean result.
+		/// </summary>
+		public string?      ExpressionPredicate { get; set; }
+
+		/// <summary>
+		/// Specifies predicate expression. This predicate will be used together with
+		/// <see cref="ThisKey"/>/<see cref="OtherKey"/> join keys, if they are specified.
+		/// Predicate expression lambda function takes two parameters: this record and other record and returns boolean result.
+		/// </summary>
+		public Expression?  Predicate           { get; set; }
+
+
+		/// <summary>
+		/// Specifies static property or method without parameters, that returns IQueryable expression. If is set, other association keys are ignored.
+		/// Result of query method should be lambda which takes two parameters: this record, IDataContext and returns IQueryable result.
+		/// <para>
+		/// <example>
+		/// <code>
+		/// public class SomeEntity
+		/// {
+		///     [Association(ExpressionQueryMethod = nameof(OtherImpl), CanBeNull = true)]
+		///     public SomeOtherEntity Other { get; set; }
+		/// 
+		///     public static Expression&lt;Func&lt;SomeEntity, IDataContext, IQueryable&lt;SomeOtherEntity&gt;&gt;&gt; OtherImpl()
+		///     {
+		///         return (e, db) =&gt; db.GetTable&lt;SomeOtherEntity&gt;().Where(se =&gt; se.Id == e.Id);
+		///     }
+		/// }
+		/// </code>
+		/// </example>
+		/// </para>
+		/// </summary>
+		public string?      QueryExpressionMethod { get; set; }
+
+		/// <summary>
+		/// Specifies query expression. If is set, other association keys are ignored.
+		/// Lambda function takes two parameters: this record, IDataContext and returns IQueryable result.
+		/// <para>
+		/// <example>
+		/// <code>
+		/// var Expression&lt;Func&lt;SomeEntity, IDataContext, IQueryable&lt;SomeOtherEntity&gt;&gt;&gt; associationQuery;
+		/// <para />
+		/// associationQuery = (e, db) =&gt; db.GetTable&lt;SomeOtherEntity&gt;().Where(se =&gt; se.Id == e.Id);
+		/// </code>
+		/// </example>
+		/// </para>
+		/// </summary>
+		public Expression?  QueryExpression       { get; set; }
+
+		/// <summary>
+		/// Specify name of property or field to store association value, loaded using <see cref="LinqExtensions.LoadWith{TEntity,TProperty}(System.Linq.IQueryable{TEntity},System.Linq.Expressions.Expression{System.Func{TEntity,TProperty}})"/> method.
+		/// When not specified, current association member will be used.
+		/// </summary>
+		public string?      Storage             { get; set; }
+
+		/// <summary>
+		/// Defines type of join:
+		/// - inner join for <c>CanBeNull = false</c>;
+		/// - left join for <c>CanBeNull = true</c>.
+		/// Default value: <c>true</c>.
+		/// </summary>
+		public bool         CanBeNull           { get; set; }
+
+		/// <summary>
+		/// This property is not used by linq2db and could be used for informational purposes.
+		/// </summary>
+		public string?      KeyName             { get; set; }
+
+		/// <summary>
+		/// This property is not used by linq2db and could be used for informational purposes.
+		/// </summary>
+		public string?      BackReferenceName   { get; set; }
+
+		/// <summary>
+		/// This property is not used by linq2db and could be used for informational purposes.
+		/// </summary>
+		public bool         IsBackReference     { get; set; }
+
+		/// <summary>
+		/// This property is not used by linq2db and could be used for informational purposes.
+		/// </summary>
+		public Relationship Relationship        { get; set; }
+
+		/// <summary>
+		/// Gets or sets alias for association. Used in SQL generation process.
+		/// </summary>
+		public string?      AliasName           { get; set; }
+
+	}
+}
---
+++ src/Limaki.UnitsOfWork.Core/Limaki.Data/Attributes/Relationship.cs
@@ -0,0 +1,24 @@
+﻿namespace Limaki.Data.Attributes
+{
+	/// <summary>
+	/// Defines relationship types for associations.
+	/// See <see cref="AssociationAttribute.Relationship"/> for more details.
+	/// </summary>
+	public enum Relationship
+	{
+		/// <summary>
+		/// One-to-one relationship.
+		/// </summary>
+		OneToOne,
+
+		/// <summary>
+		/// One-to-many relationship.
+		/// </summary>
+		OneToMany,
+
+		/// <summary>
+		/// Many-to-one relationship.
+		/// </summary>
+		ManyToOne,
+	}
+}
