d560fb28c2620bf8905a0e3697d10d9e72aebcfc
lytico <lytico@users.sourceforge.net>
2020-12-10 03:35:38 +0100

    UnitsOfWork: introduce

---
+++ src/Limaki.UnitsOfWork.Core/Limaki.Common/DateTimeUtils.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Limaki.Common {
+
+    public static class DateTimeUtils {
+        public static DateTime TruncateMilliseconds (this DateTime it) => new DateTime(it.Year, it.Month, it.Day, it.Hour, it.Minute, it.Second);
+
+    }
+
+}
