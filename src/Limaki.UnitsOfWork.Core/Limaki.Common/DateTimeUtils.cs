0f3094838ee511a47c4e11ac925bb43f4f8d5ed4
lytico <lytico@users.sourceforge.net>
2020-12-15 01:31:38 +0100

    Limaki.Common: introduce DateTimeUtils.Max

--- src/Limaki.UnitsOfWork.Core/Limaki.Common/DateTimeUtils.cs
+++ src/Limaki.UnitsOfWork.Core/Limaki.Common/DateTimeUtils.cs
@@ -3,7 +3,8 @@ using System;
 namespace Limaki.Common {
 
     public static class DateTimeUtils {
-        public static DateTime TruncateMilliseconds (this DateTime it) => new DateTime(it.Year, it.Month, it.Day, it.Hour, it.Minute, it.Second);
+        public static DateTime TruncateMilliseconds (this DateTime it) => new DateTime (it.Year, it.Month, it.Day, it.Hour, it.Minute, it.Second);
+        public static DateTime Max (this DateTime it, DateTime other) => it > other ? it : other;
 
     }
 
