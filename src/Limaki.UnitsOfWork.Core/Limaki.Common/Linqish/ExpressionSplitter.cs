025ac5276e9547c7c4f22f60e6242841df88e17e
lytico <lytico@users.sourceforge.net>
2020-12-17 06:55:28 +0100

    UnitsOfWork: ExpressionSplitter.cs: introduce Split with valid-members

--- src/Limaki.UnitsOfWork.Core/Limaki.Common/Linqish/ExpressionSplitter.cs
+++ src/Limaki.UnitsOfWork.Core/Limaki.Common/Linqish/ExpressionSplitter.cs
@@ -16,10 +16,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace Limaki.Common.Linqish {
-
 
     /// <summary>
     /// splits a <see cref="LambdaExpression"/>
@@ -30,38 +30,47 @@ namespace Limaki.Common.Linqish {
     public class ExpressionSplitter<E, M> {
 
         public bool CheckIfProperty (Expression expression) =>
-                expression is MemberExpression me //&& me.Member.MemberType == System.Reflection.MemberTypes.Property
-                && me.Expression is ParameterExpression p && p.Type == typeof (E);
+            expression is MemberExpression me //&& me.Member.MemberType == System.Reflection.MemberTypes.Property
+            && me.Expression is ParameterExpression p && p.Type == typeof(E);
 
         public MemberExpression GetProperty (Expression expression) {
             Expression result = null;
             var visitor = new ExpressionVisitVisitor ();
-            visitor.
-                VisitMemberFunc = v => {
-                    if (CheckIfProperty (v))
-                        result = v;
-                    return v;
-                };
+            visitor.VisitMemberFunc = v => {
+                if (CheckIfProperty (v))
+                    result = v;
+                return v;
+            };
             visitor.Visit (expression);
             return result as MemberExpression;
+        }
+
+        public IEnumerable<MemberExpression> GetProperties (Expression expression) {
+            var result = new Queue<MemberExpression> ();
+            var visitor = new ExpressionVisitVisitor ();
+            visitor.VisitMemberFunc = v => {
+                if (CheckIfProperty (v))
+                    result.Enqueue (v);
+                return v;
+            };
+            visitor.Visit (expression);
+            return result;
         }
 
         public int CountProperty (Expression expression) {
             int result = 0;
             var visitor = new ExpressionVisitVisitor ();
-            visitor.
-                VisitMemberFunc = v => {
-                    if (CheckIfProperty (v))
-                        result++;
-                    return v;
-                };
+            visitor.VisitMemberFunc = v => {
+                if (CheckIfProperty (v))
+                    result++;
+                return v;
+            };
 
             visitor.Visit (expression);
             return result;
         }
 
         public IEnumerable<Expression> Split (Expression<Func<E, M>> lambda) {
-
             var result = new Queue<Expression> ();
             var nodeTypes = new Queue<ExpressionType> ();
 
@@ -69,6 +78,7 @@ namespace Limaki.Common.Linqish {
             var candidates = new Stack<Expression> ();
 
             Expression Pop () => candidates.Any () ? candidates.Pop () : default;
+
             void Take () {
                 var cand = Pop ();
                 if (cand != null && CountProperty (cand) == 1)
@@ -76,9 +86,10 @@ namespace Limaki.Common.Linqish {
             }
 
             visitor.VisitBinaryFunc = v => {
-                if (v.Type == typeof (M)) {
+                if (v.Type == typeof(M)) {
                     candidates.Push (v);
                 }
+
                 visitor.BaseVisit (v.Left);
                 visitor.BaseVisit (v.Right);
 
@@ -86,7 +97,7 @@ namespace Limaki.Common.Linqish {
             };
 
             visitor.VisitMethodCallFunc = v => {
-                if (v.Type == typeof (M)) {
+                if (v.Type == typeof(M)) {
                     candidates.Push (v);
                     visitor.BaseVisit (v.Object);
                     return v;
@@ -100,16 +111,70 @@ namespace Limaki.Common.Linqish {
             while (candidates.Count != 0) {
                 Take ();
             }
+
             return result;
         }
 
+        public IEnumerable<Expression> Split (Expression<Func<E, M>> lambda, HashSet<MemberInfo> valid) {
+            bool CheckIfValid (Expression expression) =>
+                expression is MemberExpression me //&& me.Member.MemberType == System.Reflection.MemberTypes.Property
+                && me.Expression is ParameterExpression p && p.Type == typeof(E)
+                && valid.Contains (me.Member);
+
+            var visitor = new ExpressionVisitVisitor ();
+            var candidates = new Queue<Expression> ();
+
+            bool HasOnlyValidProperties (Expression expression) => GetProperties (expression).All (p => CheckIfValid (p));
+            visitor.VisitBinaryFunc = v => {
+                if (v.Type == typeof(M) && HasOnlyValidProperties (v)) {
+                    candidates.Enqueue (v);
+                    return v;
+                }
+
+                visitor.BaseVisit (v.Left);
+                visitor.BaseVisit (v.Right);
+
+                return v;
+            };
+
+            visitor.VisitMethodCallFunc = v => {
+                if (v.Type == typeof(M) && HasOnlyValidProperties (v)) {
+                    candidates.Enqueue (v);
+                    return v;
+                }
+
+                visitor.BaseVisit (v.Object);
+                return v;
+            };
+            visitor.VisitLambdaFunc = v => {
+                if (HasOnlyValidProperties (v)) {
+                    candidates.Enqueue (v);
+                    return v;
+                }
+
+                visitor.BaseVisit (v.Body);
+                return v;
+            };
+            visitor.Visit (lambda);
+            return candidates;
+        }
 
         public IEnumerable<Expression<Func<E, M>>> LambdasOfSplit (Expression<Func<E, M>> lambda) {
             foreach (var expression in Split (lambda)) {
                 yield return Expression.Lambda<Func<E, M>> (expression, lambda.Parameters);
             }
         }
-    }
 
+        public IEnumerable<Expression<Func<E, M>>> LambdasOfSplit (Expression<Func<E, M>> lambda, HashSet<MemberInfo> valid) {
+            foreach (var expression in Split (lambda, valid)) {
+                if (expression is Expression<Func<E, M>> lambdaExpression) {
+                    yield return lambdaExpression;
+                    continue;
+                }
+
+                yield return Expression.Lambda<Func<E, M>> (expression, lambda.Parameters);
+            }
+        }
+    }
 
 }
