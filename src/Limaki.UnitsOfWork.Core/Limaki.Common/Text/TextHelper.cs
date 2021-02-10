8a43fb754710341f71ca22d9a1f0f9276b432c22
lytico <lytico@users.sourceforge.net>
2021-02-10 17:43:07 +0100

    UnitsOfWork TextHelper.cs: introduce LinesOf

--- src/Limaki.UnitsOfWork.Core/Limaki.Common/Text/TextHelper.cs
+++ src/Limaki.UnitsOfWork.Core/Limaki.Common/Text/TextHelper.cs
@@ -24,9 +24,9 @@ namespace Limaki.Common.Text {
 
     public static class TextHelper {
 
-        public static bool Contains (this StringBuilder haystack, string needle) {
-            return haystack.IndexOf(needle) != -1;
-        }
+        public static bool Contains(this StringBuilder haystack, string needle) => haystack.IndexOf(needle) != -1;
+
+        public static int IndexOf(this StringBuilder haystack, string needle) => IndexOf(haystack, needle, 0);
 
         /// <summary>
         /// Simple implementation of the Knuth–Morris–Pratt algorithm that only cares about ordinal matches 
@@ -40,17 +40,13 @@ namespace Limaki.Common.Text {
         /// <param name="haystack"></param>
         /// <param name="needle"></param>
         /// <returns></returns>
-        public static int IndexOf (this StringBuilder haystack, string needle) {
-            return IndexOf(haystack, needle, 0);
-        }
-
         public static int IndexOf (this StringBuilder haystack, string needle, int startIndex) {
             if (haystack == null || needle == null)
-                throw new ArgumentNullException();
+                throw new ArgumentNullException ();
             if (needle.Length == 0)
                 return 0; //empty strings are everywhere!
             if (needle.Length == 1) { //can't beat just spinning through for it
-    
+
                 char c = needle[0];
                 for (int idx = startIndex; idx != haystack.Length; ++idx)
                     if (haystack[idx] == c)
@@ -59,7 +55,7 @@ namespace Limaki.Common.Text {
             }
             int m = startIndex;
             int i = 0;
-            int[] T = KMPTable(needle);
+            int[] T = KMPTable (needle);
             while (m < haystack.Length - 1) {
                 if (needle[i] == haystack[m + i]) {
                     if (i == needle.Length - 1)
@@ -89,82 +85,87 @@ namespace Limaki.Common.Text {
             return table;
         }
 
-        public static string Between (this StringBuilder text, string start, string end, int startIndex=0) {
-            int posStart = text.IndexOf(start, startIndex);
+        public static string Between (this StringBuilder it, string start, string end, int startIndex = 0) {
+            int posStart = it.IndexOf (start, startIndex);
             if (posStart == -1) return null;
             posStart += start.Length;
-            int posEnd = text.IndexOf(end, posStart);
+            int posEnd = it.IndexOf (end, posStart);
             if (posEnd == -1) return null;
-            return text.ToString(posStart, posEnd - posStart);
+            return it.ToString (posStart, posEnd - posStart);
         }
 
-        public static string Between (this string text, string start, string end, int startIndex=0) {
-            int posStart = text.IndexOf(start, startIndex);
+        public static string Between (this string it, string start, string end, int startIndex = 0) {
+            int posStart = it.IndexOf (start, startIndex);
             if (posStart == -1) return null;
             posStart += start.Length;
-            int posEnd = text.IndexOf(end, posStart);
+            int posEnd = it.IndexOf (end, posStart);
             if (posEnd == -1) return null;
-            return text.Substring(posStart, posEnd - posStart);
+            return it.Substring (posStart, posEnd - posStart);
         }
 
 
-        public static bool IsUnicode(byte[] buffer) {
+        public static bool IsUnicode (byte[] buffer) {
             var isUnicode = 0;
             for (int i = 1; i < buffer.Length; i += 2) {
                 if (buffer[i] == 0)
-                    isUnicode ++;
+                    isUnicode++;
             }
-            return isUnicode > buffer.Length/10;
+            return isUnicode > buffer.Length / 10;
 
         }
 
-        public static string ReplaceLeading (this string text, char leadChar, string replaceWith) {
-            if (string.IsNullOrEmpty (text))
-                return text;
+        public static string ReplaceLeading (this string it, char leadChar, string replaceWith) {
+            if (string.IsNullOrEmpty (it))
+                return it;
             var lastLead = -1;
-            for (var i = 0; i < text.Length;i++){
-                if (text [i] != leadChar) {
+            for (var i = 0; i < it.Length; i++) {
+                if (it[i] != leadChar) {
                     lastLead = i;
                     break;
                 }
             }
             if (lastLead == -1)
-                return text;
-            return String.Concat (Enumerable.Repeat (replaceWith, lastLead))+ text.Substring (lastLead);
-            
+                return it;
+            return string.Concat (Enumerable.Repeat (replaceWith, lastLead)) + it.Substring (lastLead);
+
         }
 
-        public static string ToCamelCase (string name, char delimSource, string delimDest, bool force = false) {
-            if (string.IsNullOrEmpty (name) || (!force && !name.Contains (delimSource))) {
-                return name;
+        public static string ToCamelCase (string it, char delimSource, string delimDest, bool force = false) {
+            if (string.IsNullOrEmpty (it) || (!force && !it.Contains (delimSource))) {
+                return it;
             }
-            var array = name.Split (delimSource);
+            var array = it.Split (delimSource);
             for (var i = 0; i < array.Length; i++) {
-                var s = array [i];
+                var s = array[i];
                 var first = string.Empty;
                 if (s.Length > 0) {
-                    first = Char.ToUpperInvariant (s [0]).ToString ();
+                    first = Char.ToUpperInvariant (s[0]).ToString ();
                 }
                 var rest = string.Empty;
                 if (s.Length > 1) {
                     rest = s.Substring (1).ToLowerInvariant ();
                 }
-                array [i] = first + rest;
+                array[i] = first + rest;
             }
             var newname = string.Join (delimDest, array);
             if (newname.Length == 0) {
-                newname = name;
+                newname = it;
             }
             return newname;
         }
-        public static string UnderscoreToCamelCase (string name, bool force = false) => ToCamelCase (name, '_', "", force);
+        public static string UnderscoreToCamelCase (this string it, bool force = false) => ToCamelCase (it, '_', "", force);
 
-        public static string BlankToCamelCase (string name, bool force = false) => ToCamelCase (name, ' ', "", force);
+        public static string BlankToCamelCase (this string it, bool force = false) => ToCamelCase (it, ' ', "", force);
 
         public static string GetResourceStream (Assembly assembly, string name) {
 
-            var resourceName = assembly.GetManifestResourceNames ()
-                    .SingleOrDefault (str => str == name);
+            var resources = assembly.GetManifestResourceNames ();
+            var resourceName = resources.SingleOrDefault (str => str == name);
+
+            // try harder:
+            if (resourceName == default) {
+                resourceName = resources.SingleOrDefault (str => str.EndsWith (name));
+            }
 
             if (resourceName == default)
                 return default;
@@ -174,6 +175,29 @@ namespace Limaki.Common.Text {
                 var result = reader.ReadToEnd ();
                 return result;
             }
+        }
+
+        public static IEnumerable<(string name, string content)> GetResourceStreams(Assembly assembly, Func<string,bool> acceptname) {
+
+            var resources = assembly.GetManifestResourceNames();
+            var resourceNames = resources.Where(acceptname);
+
+            foreach(var resourceName in resourceNames)
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(stream)) {
+                var result = reader.ReadToEnd();
+                yield return (resourceName, result);
+            }
+        }
+        
+        public static IEnumerable<string> LinesOf (this TextReader it) {
+
+            var line = it.ReadLine ();
+            while (line != null) {
+                yield return line;
+                line = it.ReadLine ();
+            }
+  
         }
     }
 }
