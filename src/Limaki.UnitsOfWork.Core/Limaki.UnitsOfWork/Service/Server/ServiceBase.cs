78b432eca6369b9223423b58def1aacf70cd0846
lytico <lytico@users.sourceforge.net>
2021-04-15 00:11:25 +0200

    Limaki.UnitsOfWork.Core ServiceBase.cs: remove LogWriter

--- src/Limaki.UnitsOfWork.Core/Limaki.UnitsOfWork/Service/Server/ServiceBase.cs
+++ src/Limaki.UnitsOfWork.Core/Limaki.UnitsOfWork/Service/Server/ServiceBase.cs
@@ -6,7 +6,7 @@
  * published by the Free Software Foundation.
  * 
  * Author: Lytico
- * Copyright (C) 2017 Lytico
+ * Copyright (C) 2009 - 2017 Lytico
  *
  * http://www.limada.org
  * 
@@ -16,86 +16,87 @@ using System;
 using System.IO;
 using System.Reflection;
 using Limaki.Common;
+using Limaki.Common.Reflections;
 
-namespace Limaki.UnitsOfWork.Service.Server {
+namespace Limaki.UnitsOfWork.Service.Server
+{
 
-    public class ServiceBase : IServiceBase {
-
-        public StringWriter LogWriter = new StringWriter ();
+    public class ServiceBase : IServiceBase
+    {
 
         protected ILog _logger = null;
-        protected virtual ILog Log => _logger ?? (_logger = Registry.Pool.TryGetCreate<Logger> ().Log (this.GetType ()));
+        protected virtual ILog Log => _logger ?? (_logger = Registry.Pool.TryGetCreate<Logger>().Log(GetType()));
 
         #region Exception-Handling
 
-        protected string MethodName (MethodBase method) {
+        protected string MethodName(MethodBase method)
+        {
             if (method != null)
-                return method.ToString ();
+                return method.ToString();
             else
                 return "<null>";
         }
 
-        public virtual void Catch (Exception ex) {
-            var caller = MethodName (new System.Diagnostics.StackTrace ().GetFrame (2).GetMethod ());
-            Catch (ex, caller);
+        public virtual void Catch(Exception ex)
+        {
+            var caller = MethodName(new System.Diagnostics.StackTrace().GetFrame(2).GetMethod());
+            Catch(ex, caller);
         }
 
-        public virtual void Catch (Exception ex, string msg) {
-            Log.Error (msg, ex);
-            if (this.LogWriter != null) {
-                Log.Error (this.LogWriter.ToString ());
-                this.LogWriter = new StringWriter ();
-            }
-            CatchBeforeFault?.Invoke (this, ex, msg);
+        public virtual void Catch(Exception ex, string msg)
+        {
+            Log.Error(msg, ex);
+            CatchBeforeFault?.Invoke(this, ex, msg);
             throw ex;
             //throw new FaultException<string>(msg, new FaultReason(ex.GetType().Name), new FaultCode(ex.GetType().Name), "");
             // throw new FaultException<string> (msg, ex.Message);
         }
 
         public Action<IServiceBase, Exception, string> CatchBeforeFault { get; set; }
-       
-        protected T AssertNotNull<T> (T result) {
-            var caller = MethodName (new System.Diagnostics.StackTrace ().GetFrame (2).GetMethod ());
-            return AssertNotNull (result, caller);
+
+        protected T AssertNotNull<T>(T result)
+        {
+            var caller = MethodName(new System.Diagnostics.StackTrace().GetFrame(2).GetMethod());
+            return AssertNotNull(result, caller);
         }
 
-        protected T AssertNotNull<T> (T result, string msg) {
+        protected T AssertNotNull<T>(T result, string msg)
+        {
             if (result == null)
-                Catch (new NullReferenceException (), msg);
+                Catch(new NullReferenceException(), msg);
             return result;
         }
 
         #endregion
 
-        public virtual bool Ping () {
-            var msg = this.GetType ().Name + ".Ping";
-            Log.Debug (msg);
+        public virtual bool Ping()
+        {
+            var msg = $"{GetType().FriendlyClassName()}.{nameof(Ping)}";
+            Log.Debug(msg);
             return true;
         }
 
-        public virtual string Test () {
-            var msg = this.GetType ().Name + ".Test";
-            Log.Debug (msg);
-            return null;
-        }
-
-        public virtual byte [] Resource (string name) {
-            name = @"Resources\" + name;
-            if (File.Exists (name)) {
-                using (var reader = new FileStream (name, FileMode.Open)) {
-                    var result = new byte [reader.Length];
-                    reader.Read (result, 0, (int)reader.Length);
-                    return result;
+        public virtual string TryConnect()
+        {
+            var msg = $"{GetType().FullName}.{nameof(TryConnect)}";
+            try {
+                if (Ping()) {
+                    return $"success";
                 }
+                throw new Exception($"failed");
+            } catch (Exception ex) {
+                msg = ex.ExceptionMessage(msg);
+            } finally {
+                Log.Debug(msg);
             }
-            return null;
+            return msg;
         }
 
         protected static string _serverVersion = null;
-        public virtual string ServerVersion () {
-            return _serverVersion ?? (_serverVersion = Assembly.GetAssembly (this.GetType ()).GetName ().Version.ToString (3));
-        }
+        public virtual string ServerVersion() => _serverVersion ?? (_serverVersion = Assembly.GetAssembly(GetType()).GetName().Version.ToString(3));
+
+        public virtual string ServiceInfo() => $"{GetType().FriendlyClassName()}";
 
     }
-    
+
 }
