709fe7195c5b244fd84b1c6feb733e3613960937
lytico <lytico@users.sourceforge.net>
2021-08-30 20:25:06 +0200

    add AppSettings.props & AssemblyInfo.Shared.cs

---
+++ src/AssemblyInfo.Shared.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("www.limada.org")]
+[assembly: AssemblyCopyright ("© Lytico 2006 - 2019 (http://github.com/lytico)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("20.08.0.0")]
+[assembly: AssemblyFileVersion("20.08.0.0")]
+
+
